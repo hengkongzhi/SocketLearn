ef52a5e9f804448b6b86ec81649ca33335e3637f
cyang <1522207896@qq.com>
2021-02-07 16:56:31 +0800

    0207 添加对象池

---
+++ src/HelloMemory/CELLObjectPool.hpp
@@ -0,0 +1,149 @@
+#ifndef _CELLObjectPool_hpp_
+#define _CELLObjectPool_hpp_
+#include <stdlib.h>
+#include <mutex>
+#include <assert.h>
+
+template<class Type, size_t nPoolSize>
+class CELLObjectPool
+{
+public:
+    CELLObjectPool()
+    {
+        initPool();
+    }
+    ~CELLObjectPool()
+    {
+        if (_pBuf)
+        {
+            delete[] _pBuf;
+        }
+    }
+private:
+class NodeHeader
+{
+    public:
+        NodeHeader* pNext;
+        int nID;
+        char nRef;
+        bool bPool;
+    private:
+        char c1;
+        char c2;
+    };
+    void initPool()
+    {
+        assert(nullptr == _pBuf);
+        if (_pBuf)
+        {
+            return;
+        }
+        size_t n = nPoolSize * (sizeof(Type) + sizeof(NodeHeader));
+        _pBuf = new char[n];
+        _pHeader = (NodeHeader*) _pBuf;
+        _pHeader->bPool = true;
+        _pHeader->nID = 0;
+        _pHeader->nRef = 0;
+        _pHeader->pNext = nullptr;
+        NodeHeader* pTemp1 = _pHeader;
+        for (size_t n = 1; n < nPoolSize; n++)
+        {
+            NodeHeader* pTemp2 = (NodeHeader*)(_pBuf + n * (sizeof(Type) + sizeof(NodeHeader)));
+            pTemp2->bPool = true;
+            pTemp2->nID = n;
+            pTemp2->nRef = 0;
+            pTemp2->pNext = nullptr;
+            pTemp1->pNext = pTemp2;
+            pTemp1 = pTemp2;
+        }
+    }
+public:
+
+
+    void* allocObjMemory(size_t nSize)
+    {
+        std::lock_guard<std::mutex> lg(_mutex);
+        if (!_pBuf)
+        {
+            initPool();
+        }
+        NodeHeader* pReturn = nullptr;
+        if (nullptr == _pHeader)
+        {
+            pReturn = (NodeHeader*) new char[(sizeof(Type) + sizeof(NodeHeader))];
+            pReturn->bPool = false;
+            pReturn->nID = -1;
+            pReturn->nRef = 1;
+            pReturn->pNext = nullptr;
+        }
+        else
+        {
+            pReturn = _pHeader;
+            _pHeader = _pHeader->pNext;
+            assert(0 == pReturn->nRef);
+            pReturn->nRef = 1;
+        }
+        printf("allocObjMemory: %llx, id=%d, size=%d\n", pReturn, pReturn->nID, nSize);
+        return ((char*) pReturn + sizeof(NodeHeader));
+    }
+    void freeObjMemory(void* pMem)
+    {
+        NodeHeader* pBlock = (NodeHeader*) ((char*) pMem - sizeof(NodeHeader));
+        assert(1 == pBlock->nRef);
+        if (pBlock->bPool)
+        {
+            std::lock_guard<std::mutex> lg(_mutex);
+            if (--pBlock->nRef != 0)
+            {
+                return;
+            }
+            pBlock->pNext = _pHeader;
+            _pHeader = pBlock;
+        }
+        else
+        {
+            if (--pBlock->nRef != 0)
+            {
+                return;
+            }
+            delete[] pBlock;
+        }
+    }
+
+private:
+    NodeHeader* _pHeader;
+    char* _pBuf;
+    std::mutex _mutex;
+
+
+};
+
+
+template<class Type>
+class ObjectPoolBase
+{
+public:
+    void* operator new(size_t nSize)
+    {
+        return malloc(nSize);
+    }
+    void operator delete(void* p)
+    {
+        free(p);
+    }
+    template<typename ...Args>
+    static Type* createObject(Args ...args)
+    {
+        Type* obj = new Type(args...);
+        return obj;
+    }
+    static void destroyObject(Type* obj)
+    {
+        delete obj;
+    }
+private:
+};
+
+
+
+#endif
--- src/HelloMemory/main.cpp
+++ src/HelloMemory/main.cpp
@@ -3,9 +3,10 @@
 #include <thread>
 #include "../CELLTimestamp.hpp"
 #include "Alloctor.h"
+#include "CELLObjectPool.hpp"
 
 using namespace std;
-class classA
+class classA:public ObjectPoolBase<classA>
 {
 public:
     classA(int a)
@@ -21,37 +22,53 @@ public:
     int num = 0;
 
 };
-shared_ptr<classA> fun(shared_ptr<classA> pA)
+class classB:public ObjectPoolBase<classB>
 {
-    pA->num++;
-    shared_ptr<classA> pB = pA;
-    return pB;
-}
-classA* fun(classA* pA)
-{
-    pA->num++;
-    classA* pB = pA;
-    return pB;
-}
-
-const int tCount = 8;
-const int mCount = 100000;
-const int nCount = mCount / tCount;
-char* data[mCount];
-void workFun(int index)
-{
-    size_t begin = index * nCount;
-    size_t end = (index + 1) * nCount;
-    for (size_t i = begin; i < end; i++)
+public:
+    classB(int a, int b)
     {
-        data[i] = new char [1 + (rand() % 1024)];
+        num = a * b;
+        printf("classB\n");
     }
-    for (size_t i = begin; i < end; i++)
+    ~classB()
     {
-        delete[] data[i];
+        printf("~classB\n");
     }
+public:
+    int num = 0;
 
-}
+};
+// shared_ptr<classA> fun(shared_ptr<classA> pA)
+// {
+//     pA->num++;
+//     shared_ptr<classA> pB = pA;
+//     return pB;
+// }
+// classA* fun(classA* pA)
+// {
+//     pA->num++;
+//     classA* pB = pA;
+//     return pB;
+// }
+
+// const int tCount = 8;
+// const int mCount = 100000;
+// const int nCount = mCount / tCount;
+// char* data[mCount];
+// void workFun(int index)
+// {
+//     size_t begin = index * nCount;
+//     size_t end = (index + 1) * nCount;
+//     for (size_t i = begin; i < end; i++)
+//     {
+//         data[i] = new char [1 + (rand() % 1024)];
+//     }
+//     for (size_t i = begin; i < end; i++)
+//     {
+//         delete[] data[i];
+//     }
+
+// }
 int main()
 {
     // thread t[tCount];
@@ -74,26 +91,31 @@ int main()
     // printf("%d\n", *b);
     // classA* a1 = new classA();
     // delete a1;
-    {
-        shared_ptr<classA> b = make_shared<classA>(100);
-        CELLTimestamp tTime;
-        for (int i = 0; i < 10000000; i++)
-        {
-            shared_ptr<classA> c = fun(b);
-        }
-        cout << tTime.getElapsedTimeInMilliSec() << endl;
-    }
+    // {
+    //     shared_ptr<classA> b = make_shared<classA>(100);
+    //     CELLTimestamp tTime;
+    //     for (int i = 0; i < 10000000; i++)
+    //     {
+    //         shared_ptr<classA> c = fun(b);
+    //     }
+    //     cout << tTime.getElapsedTimeInMilliSec() << endl;
+    // }
 
-    {
-        classA* b = new classA(100);
-        CELLTimestamp tTime;
-        for (int i = 0; i < 10000000; i++)
-        {
-            classA* c = fun(b);
-        }
-        cout << tTime.getElapsedTimeInMilliSec() << endl;
-        delete b;
-    }
-
+    // {
+    //     classA* b = new classA(100);
+    //     CELLTimestamp tTime;
+    //     for (int i = 0; i < 10000000; i++)
+    //     {
+    //         classA* c = fun(b);
+    //     }
+    //     cout << tTime.getElapsedTimeInMilliSec() << endl;
+    //     delete b;
+    // }
+    classA* a1 = new classA(5);
+    delete a1;
+    classA* a2 = classA::createObject(6);
+    classA::destroyObject(a2);
+    classB* b1 = classB::createObject(5, 6);
+    classB::destroyObject(b1);
     return 0;
 }
