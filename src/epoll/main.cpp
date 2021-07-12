fadb540131e7a56804182d6bf7588e133caddcf8
cyang <1522207896@qq.com>
2021-07-12 23:02:35 +0800

    学习epoll

--- src/epoll/main.cpp
+++ src/epoll/main.cpp
@@ -36,10 +36,38 @@ int main()
     ev.events = EPOLLIN;
     ev.data.fd = sock;
     epoll_ctl(epfd, EPOLL_CTL_ADD, sock, &ev);
+
+    //用于接收检测到网络事件的数组
+    epoll_event events[256] = {};
     while (true)
     {
-
+        int n = epoll_wait(epfd, events, 256, 0);
+        if (n < 0)
+        {
+            printf("error,epoll_wait ret=%d\n", n);
+            break;
+        }
+        for (int i = 0; i < n; i++)
+        {
+            if (events[i].data.fd == sock)
+            {
+                sockaddr_in clientAddr = {};
+                int nAddrLen = sizeof(sockaddr_in);
+                SOCKET _cSock = INVALID_SOCKET;
+                _cSock = accept(sock, (sockaddr*)&clientAddr, (socklen_t*)&nAddrLen);
+                if (INVALID_SOCKET == _cSock)
+                {
+                    printf("错误,接受到无效客户端SOCKET...\n");
+                }
+                else
+                {
+                    printf("新客户端加入：socket = %d,IP = %s\n", (int)_cSock, inet_ntoa(clientAddr.sin_addr));
+                }
+            }
+        }
     }
-
+    close(sock);
+    close(epfd);
+    printf("已退出。。。\n");
     return 0;
 }
