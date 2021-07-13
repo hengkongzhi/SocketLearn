9c0b3260e9f2bb16a55af02a7b3028a6d4b2b00b
cyang <1522207896@qq.com>
2021-07-13 23:37:56 +0800

    0713 封装epoll_ctl

--- src/epoll/main.cpp
+++ src/epoll/main.cpp
@@ -2,13 +2,43 @@
 #include <arpa/inet.h>
 #include <string.h>
 #include <sys/epoll.h>
+#include <thread>
 #define SOCKET int
 #define INVALID_SOCKET (SOCKET)(~0)
 #define SOCKET_ERROR (-1)
 #include <stdio.h>
-
+#include <vector>
+std::vector<SOCKET> g_clients;
+bool g_bRun = true;
+void cmdThread()
+{
+    while (true)
+    {
+        char cmdBuf[256] = {0};
+        scanf("%s", cmdBuf);
+        if (0 == strcmp(cmdBuf, "exit"))
+        {
+            g_bRun = false;
+            printf("退出cmdThread线程\n");
+            break;
+        }
+        else
+        {
+            printf("不支持的命令。\n");
+        }
+    }
+}
+int cellEpollCtl(int epfd, int op, SOCKET sockfd, uint32_t events)
+{
+    epoll_event ev;
+    ev.events = events;
+    ev.data.fd = sockfd;
+    epoll_ctl(epfd, op, sockfd, &ev);
+}
 int main()
 {
+    std::thread t1(cmdThread);
+    t1.detach();
     SOCKET sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
     sockaddr_in _sin = {};
     _sin.sin_family = AF_INET;
@@ -32,14 +62,11 @@ int main()
     }
     //linux 2.6.8后size这个值没有意义了
     int epfd = epoll_create(256);
-    epoll_event ev;
-    ev.events = EPOLLIN;
-    ev.data.fd = sock;
-    epoll_ctl(epfd, EPOLL_CTL_ADD, sock, &ev);
+    cellEpollCtl(epfd, EPOLL_CTL_ADD, sock, EPOLLIN);
 
     //用于接收检测到网络事件的数组
     epoll_event events[256] = {};
-    while (true)
+    while (g_bRun)
     {
         int n = epoll_wait(epfd, events, 256, 0);
         if (n < 0)
@@ -61,10 +88,20 @@ int main()
                 }
                 else
                 {
+                    g_clients.push_back(_cSock);
+                    cellEpollCtl(epfd, EPOLL_CTL_ADD, _cSock, EPOLLIN);
                     printf("新客户端加入：socket = %d,IP = %s\n", (int)_cSock, inet_ntoa(clientAddr.sin_addr));
                 }
             }
+            else if (events[i].events & EPOLLIN)
+            {
+                printf("收到客户端数据：socket = %d\n", events[i].data.fd);
+            }
         }
+    }
+    for (auto client : g_clients)
+    {
+        close(client);
     }
     close(sock);
     close(epfd);
